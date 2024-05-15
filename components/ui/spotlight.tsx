f40a996b9ccaf306b67556b34e1db71f4e5f42c1
Scott Jones <info@netgurusolutions.co.uk>
2024-05-15 12:21:01 +0100

    height fix

--- components/ui/spotlight.tsx
+++ components/ui/spotlight.tsx
@@ -10,7 +10,7 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
   return (
     <svg
       className={cn(
-        "animate-spotlight pointer-events-none absolute z-[1]  md:h-[210%] md:w-[245%] lg:w-[98%] opacity-0",
+        "animate-spotlight pointer-events-none absolute z-[1] md:h-[100%] md:w-[245%] lg:w-[98%] opacity-0",
         className
       )}
       xmlns="http://www.w3.org/2000/svg"
